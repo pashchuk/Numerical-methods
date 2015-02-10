bae3e2f2a084a9607b11846ac847ed43c8d7069e
Eduard <mamaxropela@gmail.com>
2015-02-10 23:06:02 +0200

    create matrix loader class

--- src/lab1.py
+++ src/lab1.py
@@ -1,1 +1,36 @@
 __author__ = 'Eduard'
+import sys
+
+class MatrixLoader:
+
+    def __init__(self,mode='file',filename='input.txt'):
+        self.mode = mode
+        self.fileName = filename
+        self.matrix = []
+
+    def getMatrix(self):
+        if self.mode == 'file':
+            self.__getFromFile()
+        else:
+            self.__getFromConsole()
+        return self.matrix
+
+    def __getFromFile(self):
+        with open(self.fileName,'r') as file:
+            for line in file:
+                # self.matrix.append([int(x) for x in line.strip().split(' ')])
+                self.matrix.append(map(lambda x: int(x), line.strip().split(' ')))
+
+    def __getFromConsole(self):
+        print 'Input matrix'
+        line = raw_input()
+        while len(line) != 0:
+            self.matrix.append(map(lambda x: int(x),line.strip().split(' ')))
+            line = raw_input()
+
+
+
+
+loader = MatrixLoader(mode='console')
+matrix = loader.getMatrix()
+print matrix
