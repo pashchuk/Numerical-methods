e7535cd2c7b62168d8425e6c203a5b42d7d8f021
Eduard <mamaxropela@gmail.com>
2015-03-02 21:21:25 +0200

    add lab1

--- FSharp/lab1/Program.fs
+++ FSharp/lab1/Program.fs
@@ -1,7 +1,25 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open System.IO
+open System.Windows.Forms
 
+
+type row = float list * float
+
+[<STAThread>]
 [<EntryPoint>]
-let main argv = 
+let main argv =
+    
+    let dialog = new OpenFileDialog()
+    dialog.ShowDialog() |> ignore
+    let data = 
+        try 
+            dialog.FileName 
+            |> File.ReadAllLines 
+            |> Array.map (fun x -> x.Split ' ') 
+            |> Array.map (fun x -> x |> Array.map Double.Parse)
+        with _ as err -> failwith err.Message
+    for row in 0..text.Length-1 do
+        let index = 
     printfn "%A" argv
+    Console.ReadLine()
     0 // return an integer exit code
