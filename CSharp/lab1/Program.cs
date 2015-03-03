7c14584cb8e1fe319c02ff0da9d0a115eeb25622
Eduard <mamaxropela@gmail.com>
2015-03-03 23:21:19 +0200

    add ctors

--- CSharp/lab1/Program.cs
+++ CSharp/lab1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace lab1
 {
@@ -12,8 +14,49 @@ namespace lab1
 
 		public static void Main(string[] args)
 		{
-
+			var gaus = new Gaus("input.txt");
 		}
+
+		 public class Gaus
+		 {
+			 public double[,] Matrix { get; private set; }
+			 public string Path { get; private set; }
+
+			 public Gaus(int size, double fillValue = 1.0)
+			 {
+				 Matrix = new double[size, size];
+				 for (var i = 0; i < size; i++)
+					 for (var j = 0; j < size; j++)
+						 Matrix[i, j] = fillValue;
+			 }
+
+			 public Gaus(string path)
+			 {
+				 try
+				 {
+					 var lines = File.ReadAllLines(path)
+						 .Select(x => x.Split(' '))
+						 .Select(x => x.Select(double.Parse).ToArray())
+						 .ToArray();
+					 var size = lines.Length;
+					 Matrix = new double[size, size];
+					 for(var i = 0; i < size; i++)
+						 for (var j = 0; j < size; j++)
+							 Matrix[i, j] = lines[i][j];
+					 Path = Environment.CurrentDirectory + "\\" + path;
+
+				 }
+				 catch (FileNotFoundException ex)
+				 {
+					 MessageBox.Show(ex.Message, "File not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				 }
+			 }
+
+			 public void SolveGaus()
+			 {
+				 
+			 }
+		 }
 	}
 
 }
