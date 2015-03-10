57ce3918e934a5e6b2b39e09806363eaec1a5e76
Eduard <mamaxropela@gmail.com>
2015-03-10 21:06:31 +0200

    add swap methods

--- CSharp/lab1/Matrix.cs
+++ CSharp/lab1/Matrix.cs
@@ -21,7 +21,8 @@ namespace lab1
 			get { return _matrix[row, column]; }
 			set { _matrix[row, column] = value; }
 		}
-
+		public int Rows { get { return _matrix.GetLength(0); } }
+		public int Columns { get { return _matrix.GetLength(1); } }
 		#endregion
 
 		#region Costructors
@@ -43,8 +44,26 @@ namespace lab1
 
 		#region public Methods
 
-
-
+		public void SwapRows(int row1, int row2)
+		{
+			T tempvariable;
+			for (int i = 0; i < Columns; i++)
+			{
+				tempvariable = _matrix[row1, i];
+				_matrix[row1, i] = _matrix[row2, i];
+				_matrix[row2, i] = tempvariable;
+			}
+		}
+		public void SwapColumns(int column1, int column2)
+		{
+			T tempvariable;
+			for (int i = 0; i < Rows; i++)
+			{
+				tempvariable = _matrix[i, column1];
+				_matrix[i, column1] = _matrix[i, column2];
+				_matrix[i, column2] = tempvariable;
+			}
+		}
 		#endregion
 
 		#region Events
