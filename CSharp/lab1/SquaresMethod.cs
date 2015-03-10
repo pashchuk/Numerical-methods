216aea9df69286e8ae0e256f2f4cba3e809c414e
Eduard <mamaxropela@gmail.com>
2015-03-10 21:50:31 +0200

    add class that will be solve squares method equation

---
+++ CSharp/lab1/SquaresMethod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+	public class SquaresMethod
+	{
+		#region Fields
+
+		private Matrix<double> _matrix; 
+
+		#endregion
+
+		#region Properties
+
+
+
+		#endregion
+
+		#region Costructors
+
+		public SquaresMethod(Matrix<double> matrix)
+		{
+			_matrix = matrix;
+		}
+
+		#endregion
+
+		#region private Methods
+
+
+
+		#endregion
+
+		#region public Methods
+
+		public void Solve()
+		{
+			
+		}
+
+		#endregion
+
+		#region Events
+
+
+
+		#endregion
+	}
+}
