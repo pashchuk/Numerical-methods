8845f032d8d681fe843ae38bacc80a7eea3fe2a1
Eduard <mamaxropela@gmail.com>
2015-03-10 20:53:35 +0200

    reconfigure class structure

--- CSharp/lab1/Matrix.cs
+++ CSharp/lab1/Matrix.cs
@@ -8,6 +8,45 @@ namespace lab1
 {
 	public class Matrix<T> where T : struct 
 	{
-		
+		#region Fields
+
+		private T[,] _matrix;
+
+		#endregion
+
+		#region Properties
+
+
+
+		#endregion
+
+		#region Costructors
+
+		public Matrix(int rows, int columns)
+		{
+			_matrix = new T[rows, columns];
+		}
+
+		public Matrix(int size) : this(size, size) { }
+
+		#endregion
+
+		#region private Methods
+
+
+
+		#endregion
+
+		#region public Methods
+
+
+
+		#endregion
+
+		#region Events
+
+
+
+		#endregion
 	}
 }
