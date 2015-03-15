c84125167ad1c5dad3219479764067da5f998fe1
Eduard <mamaxropela@gmail.com>
2015-03-15 22:31:06 +0200

    end format output

--- CSharp/Nums/SeidelMethod.cs
+++ CSharp/Nums/SeidelMethod.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Nums
 		private Matrix<double> _matrix;
 		private double[] _vector;
 		private double[] _solution;
+		private List<IterationData> _iterationsData; 
 
 		#endregion
 
@@ -29,6 +31,7 @@ namespace Nums
 		{
 			_matrix = inputMatrix;
 			_vector = vector;
+			_iterationsData = new List<IterationData>();
 		}
 
 		#endregion
@@ -42,8 +45,10 @@ namespace Nums
 			var currentSolution = new double[size];
 			var previousSolution = new double[size];
 			var iterationCount = 0;
+			//start calculations
 			do
 			{
+				//copy finded solution to previous solution array
 				Array.Copy(currentSolution, previousSolution, size);
 				for (int i = 0; i < size; i++)
 				{
@@ -54,8 +59,25 @@ namespace Nums
 						sum += previousSolution[j]*matrix[i, j];
 					currentSolution[i] = (vector[i] - sum)/matrix[i, i];
 				}
-				iterationCount++;
-			} while (!isEnd(eps: epsilon, curr: currentSolution, prev: previousSolution));
+				//save data about the first three iterations
+				if (++iterationCount <= 3)
+				{
+					var copyCurrSol = new double[size];
+					Array.Copy(currentSolution, copyCurrSol, size);
+					IterationData iterationData;
+					iterationData.solution = copyCurrSol;
+					iterationData.difference = getDifferenceVector(currentSolution);
+					iterationData.iterId = iterationCount;
+					_iterationsData.Add(iterationData);
+				}
+			} while (!isEnd(eps: epsilon, curr: currentSolution, prev: previousSolution) || iterationCount == int.MaxValue);
+			//save data about pre-last iteration
+			IterationData data;
+			data.solution = previousSolution;
+			data.difference = getDifferenceVector(previousSolution);
+			data.iterId = iterationCount - 1;
+			_iterationsData.Add(data);
+			//save last losultion
 			_solution = currentSolution;
 		}
 
@@ -79,19 +101,53 @@ namespace Nums
 			return verifyVector;
 		}
 
+		private double[] getDifferenceVector(double[] resultVector)
+		{
+			var res = verify(_matrix, resultVector);
+			var difference = new double[resultVector.Length];
+			for (int i = 0; i < res.Length; i++)
+				difference[i] = _vector[i] - res[i];
+			return difference;
+		}
+
 		#endregion
 
 		#region public Methods
 
 		public void Solve()
 		{
-			calculate(_matrix, _vector);
-			foreach (var d in _solution)
-				Console.WriteLine(d);
+			Console.WriteLine("------ Input matrix ------");
+			_matrix.Print(Console.Out);
 			Console.WriteLine();
+			Console.WriteLine("------ Input vector ------");
+			foreach (var d in _vector)
+				Console.Write("{0:0.000}  ", d);
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("------ Result vector -----");
+			calculate(_matrix, _vector);
+			foreach (var a in _solution)
+				Console.WriteLine("{0,10:0.0000000}", a);
+			Console.WriteLine();
+			Console.WriteLine("---- difference vector ---");
 			var ver = verify(_matrix, _solution);
-			foreach (var d in ver)
-				Console.WriteLine(d);
+			for (int i = 0; i < ver.Length; i++)
+				Console.WriteLine(_vector[i] - ver[i]);
+			Console.WriteLine();
+			Console.WriteLine("---- Iterations data -----");
+			foreach (var iterationData in _iterationsData)
+			{
+				Console.WriteLine("Iteration # {0,3} --\n", iterationData.iterId);
+				Console.Write("   -- vector --\n   ");
+				foreach (var sol in iterationData.solution)
+					Console.Write("{0,10:0.0000000}  ", sol);
+				Console.WriteLine("\n");
+				Console.Write("   - difference -\n   ");
+				foreach (var d in iterationData.difference)
+					Console.Write("{0,10:0.0000000}  ", d);
+				Console.WriteLine();
+				Console.WriteLine();
+			}
 		}
 
 		#endregion
@@ -101,5 +157,12 @@ namespace Nums
 
 
 		#endregion
+
+		struct IterationData
+		{
+			public int iterId;
+			public double[] solution;
+			public double[] difference;
+		}
 	}
 }
