b88bc7386f354cd9bde4db21551f84ccb9fcdba3
Eduard <mamaxropela@gmail.com>
2015-03-15 20:11:37 +0200

    complete writing seidel iteration function

--- CSharp/Nums/SeidelMethod.cs
+++ CSharp/Nums/SeidelMethod.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,7 +34,34 @@ namespace Nums
 
 		#region private Methods
 
-		
+		private void calculate(Matrix<double> matrix, double[] vector)
+		{
+			const double epsilon = 0.0000001;
+			var size = vector.Length;
+			var currentSolution = new double[size];
+			var previousSolution = new double[size];
+			var iterationCount = 0;
+			do
+			{
+				Array.Copy(currentSolution, previousSolution, size);
+				for (int i = 0; i < size; i++)
+				{
+					var sum = default (double);
+					for (int j = 0; j < i; j++)
+						sum += currentSolution[j]*matrix[i, j];
+					for (int j = i + 1; j < size; j++)
+						sum += previousSolution[j]*matrix[i, j];
+					currentSolution[i] = (vector[i] - sum)/matrix[i, i];
+				}
+				iterationCount++;
+			} while (isEnd(eps: epsilon, curr: currentSolution, prev: previousSolution));
+		}
+
+		private bool isEnd(double[] curr, double[] prev, double eps)
+		{
+			var sum = curr.Select((cur, i) => (cur - prev[i])*(cur - prev[i])).Sum();
+			return Math.Sqrt(sum) <= eps;
+		}
 
 		#endregion
 
